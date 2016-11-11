d296718ee5f33ce7fee9094d0e20a810729943eb
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-11-10 23:11:49 -1000

    finished

--- calculator.js
+++ calculator.js
@@ -7,11 +7,25 @@
  */
 
 
+function calculatorModule(){
+  var memory = 0;
+  var total = 0;
+
+
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
+
+   function getLoad(x){
+    if(typeof x === 'number'){
+    total = x;
+    return total;
+    } else {
+      throw new Error();
+    }
+   }
 
 
   /**
@@ -19,11 +33,23 @@
    * @return { Number }
    */
 
+   function theTotal(){
+    return total;
+   }
+
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
+
+   function sum(x){
+    if(typeof x === 'number'){
+    total += x;
+    } else {
+      throw new Error();
+    }
+   }
 
 
   /**
@@ -31,11 +57,27 @@
    * @param  { Number } x
    */
 
+   function difference(x){
+    if(typeof x === 'number'){
+    total -= x;
+    } else {
+      throw new Error();
+    }
+   }
+
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
+
+   function product(x){
+    if(typeof x === 'number'){
+    total *= x;
+    } else {
+      throw new Error();
+    }
+   }
 
 
   /**
@@ -43,23 +85,61 @@
    * @param  { Number } x
    */
 
+   function quotient(x){
+    if(typeof x === 'number'){
+    total /= x;
+    } else {
+      throw new Error();
+    }
+   }
+
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
+   function theMemory(){
+    return memory;
+   }
+
+
+
 
   /**
    * Stores the value of `total` to `memory`
    */
+
+   function savedMemory(){
+    memory = total;
+   }
+
 
 
   /**
    * Clear the value stored at `memory`
    */
 
+   function clearMemory(){
+    memory = 0;
+   }
+
   /**
    * Validation
-   */
+     */
 
+
+
+  return {
+    load: getLoad,
+    getTotal: theTotal,
+    add: sum,
+    subtract: difference,
+    multiply: product,
+    divide: quotient,
+    recallMemory: theMemory,
+    saveMemory: savedMemory,
+    clearMemory: clearMemory
+
+  };
+ }
